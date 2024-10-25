7046d386d2c1d17fdf50ea50b73d001bf7b083d2
Muhammaddiyor Tohirov <mtohirov60@gmail.com>
2024-10-25 16:05:22 +0500

    chore: format code with prettier

--- src/utils/cn.ts
+++ src/utils/cn.ts
@@ -1,10 +1,10 @@
-import type { ClassValue } from 'clsx';
+import type { ClassValue } from 'clsx'
 
-import { clsx } from 'clsx';
-import { twMerge } from 'tailwind-merge';
+import clsx from 'clsx'
+import { twMerge } from 'tailwind-merge'
 
-export function cn(...classNames: ClassValue[]) {
-  return twMerge(clsx(classNames));
+export function cn(...classNames: ClassValue[]): string {
+  return twMerge(clsx(classNames))
 }
 
-export default cn;
+export default cn
